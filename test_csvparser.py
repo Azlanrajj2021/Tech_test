2e8ef7435d33dd683b0cadf9202881e8e4103746
Azlan <azlanrajj@gmail.com>
2022-03-07 17:30:37 +0000

    Test Update

--- test_csvparser.py
+++ test_csvparser.py
@@ -1,14 +1,14 @@
 import unittest
 from unittest.mock import Mock
-
 import csvparser
-
 
 class TestMethods(unittest.TestCase):
     def test_read_row(self):
         file = Mock()
-        file.configure_mock(**{'readline.return_value': "\"First\",\"Second\""})
-        assert csvparser.read_row(file) == ['First','Second']
+        file.configure_mock(**{'readline.return_value': "First,Second"})
+        actual = csvparser.read_row(file)
+        expected = 'First - Second'
+        assert actual == expected
 
 
 if __name__ == "__main__":
